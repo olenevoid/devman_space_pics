7c50d4d678d1c9de2d154c5d8a83ed82186e9a07
olenevoid <mazekunov@gmail.com>
2025-05-23 11:09:34 +0500

    Make fetch_nasa_epic_images function private

--- nasa/fetch_epic_images.py
+++ nasa/fetch_epic_images.py
@@ -40,7 +40,7 @@ def get_nasa_epic_image_urls(api_key: str, date: str):
     return image_urls
 
 
-def fetch_nasa_epic_images(api_key: str, limit: int | None = None):
+def _fetch_nasa_epic_images(api_key: str, folder, limit: int | None = None):
     makedirs(folder, exist_ok=True)
     
     last_date = get_last_nasa_epic_date_with_photos(api_key)
