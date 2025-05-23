802f50206d8c5ca47542c315f89d41c41f91130f
olenevoid <mazekunov@gmail.com>
2025-05-23 11:11:12 +0500

    Add wrapper functions to nasa/__init__.py

--- nasa/__init__.py
+++ nasa/__init__.py
@@ -5,3 +5,11 @@ from .fetch_epic_images import _fetch_nasa_epic_images
 
 
 NASA_FOLDER = path.join(IMAGE_FOLDER_NAME, 'nasa')
+
+
+def fetch_apod_images(api_key, count, folder=NASA_FOLDER):
+    return _fetch_nasa_apod_images(api_key, count, folder)
+
+
+def fetch_epic_images(api_key, limit, folder=NASA_FOLDER):
+    return _fetch_nasa_epic_images(api_key, folder, limit)
