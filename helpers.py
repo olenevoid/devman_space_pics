323cb0da325ee50736f6ef34669aa55a3b4c60b3
olenevoid <mazekunov@gmail.com>
2025-05-23 09:19:20 +0500

    Create helpers.py

---
+++ helpers.py
@@ -0,0 +1,25 @@
+import requests
+from urllib.parse import urlparse, unquote
+from os import path
+
+
+def save_image(url: str, filename: str, params: dict = None):
+    
+    response = requests.get(url, params=params)
+
+    response.raise_for_status()
+
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+
+
+def get_filename_from_url(url):
+    parsed_url = urlparse(url)
+
+    name = parsed_url.path.split('/')[-1]
+    name = unquote(name)
+
+    if path.splitext(name)[1] == '':
+        name = name+'.jpeg'
+
+    return name
