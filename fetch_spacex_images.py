4eab81f8b565c70f20ab6fd7ee557d3b2930f2d3
olenevoid <mazekunov@gmail.com>
2025-05-23 10:09:48 +0500

    Make function get_spacex_image_urls private

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -6,7 +6,7 @@ from os import path, makedirs
 SPACEX_FOLDER = path.join(IMAGE_FOLDER_NAME, 'spacex')
 
 
-def get_spacex_image_urls(launch_id: str):
+def _get_spacex_image_urls(launch_id: str):
     url = f'https://api.spacexdata.com/v5/launches/{launch_id}'
     response = requests.get(url)
     response.raise_for_status()
@@ -19,8 +19,7 @@ def get_spacex_image_urls(launch_id: str):
 def fetch_spacex_images(launch_id: str):
     makedirs(SPACEX_FOLDER, exist_ok=True)
 
-    image_urls = get_spacex_image_urls(launch_id)
+    image_urls = _get_spacex_image_urls(launch_id)
     for image_url in image_urls:
         filename = path.join(SPACEX_FOLDER, get_filename_from_url(image_url))
         save_image(image_url, filename)
-        
