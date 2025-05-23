e763f9aa9f11ee68d23471bf5f55188464cdb55c
olenevoid <mazekunov@gmail.com>
2025-05-23 09:55:16 +0500

    Delete spacex functions from main

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 from helpers import save_image, get_filename_from_url, IMAGE_FOLDER_NAME
 
 
-SPACEX_FOLDER = path.join(IMAGE_FOLDER_NAME, 'spacex')
 NASA_FOLDER = path.join(IMAGE_FOLDER_NAME, 'nasa')
 
 
@@ -62,25 +61,6 @@ def get_nasa_epic_image_urls(api_key: str, date: str):
 
     return image_urls
   
-
-def get_spacex_image_urls(launch_id: str):
-    url = f'https://api.spacexdata.com/v5/launches/{launch_id}'
-    response = requests.get(url)
-    response.raise_for_status()
-
-    json_dict: dict = response.json()
-
-    return json_dict['links']['flickr']['original']
-
-
-def fetch_spacex_images(launch_id: str):
-    makedirs(SPACEX_FOLDER, exist_ok=True)
-
-    image_urls = get_spacex_image_urls(launch_id)
-    for image_url in image_urls:
-        filename = path.join(SPACEX_FOLDER, get_filename_from_url(image_url))
-        save_image(image_url, filename)
-
 
 def fetch_nasa_apod_images(api_key: str, count: int):
     makedirs(NASA_FOLDER, exist_ok=True)
