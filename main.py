a5ea2070a6ae4274ea18ee7b002ad5970b38f62c
olenevoid <mazekunov@gmail.com>
2025-05-23 11:12:02 +0500

    Switch to nasa package import instead of individual scripts

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from os import makedirs, environ
 from dotenv import load_dotenv
 from helpers import IMAGE_FOLDER_NAME
 from fetch_spacex_images import fetch_spacex_images
-from nasa.fetch_apod_images import fetch_nasa_apod_images
-from nasa.fetch_epic_images import fetch_nasa_epic_images
-
+import nasa
 
 def main():
     load_dotenv()
@@ -12,8 +10,8 @@ def main():
     makedirs(IMAGE_FOLDER_NAME, exist_ok=True)
     
     fetch_spacex_images('605b4b95aa5433645e37d041')
-    fetch_nasa_apod_images(nasa_api_key, 3)    
-    fetch_nasa_epic_images(nasa_api_key, 3)
+    nasa.fetch_apod_images(nasa_api_key, 3)    
+    nasa.fetch_epic_images(nasa_api_key, 3)
     
 
 if __name__ == '__main__':
