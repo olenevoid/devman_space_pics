b7e0fea1089cf5cfe241ecad9fc21ccdb06e6382
olenevoid <mazekunov@gmail.com>
2025-05-23 11:10:05 +0500

    Rename imports inside nasa

--- nasa/__init__.py
+++ nasa/__init__.py
@@ -1,5 +1,7 @@
 from helpers import IMAGE_FOLDER_NAME
 from os import path
+from .fetch_apod_images import _fetch_nasa_apod_images
+from .fetch_epic_images import _fetch_nasa_epic_images
 
 
 NASA_FOLDER = path.join(IMAGE_FOLDER_NAME, 'nasa')
