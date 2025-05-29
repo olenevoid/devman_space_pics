c46bacc3aca19dfc51def714b8cb70a0c353929f
olenevoid <mazekunov@gmail.com>
2025-05-29 15:05:58 +0500

    Create telegram_bot.py

---
+++ telegram_bot.py
@@ -0,0 +1,24 @@
+from os import environ
+from dotenv import load_dotenv
+from telegram import Bot
+
+
+class ImagePosterBot():
+    def __init__(self):
+        load_dotenv()
+        self.tg_bot_token = environ['TG_BOT_TOKEN']
+        self.tg_channel_id = environ['TG_CHANNEL_ID']
+        self.bot = Bot(self.tg_bot_token)
+
+    def send_image(self, image_path):
+        image = open(image_path, 'rb')
+        self.bot.send_photo(chat_id=self.tg_channel_id, photo=image) 
+    
+
+def main():    
+    bot = ImagePosterBot().bot
+    print(bot.get_me())
+
+
+if __name__ == '__main__':
+    main()
