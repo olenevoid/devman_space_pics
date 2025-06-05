f1ffde4b94d8c1cc59513117891483109d237f58
olenevoid <mazekunov@gmail.com>
2025-06-05 15:57:43 +0500

    Delete ifmain in telegram_bot

--- telegram_bot.py
+++ telegram_bot.py
@@ -8,12 +8,3 @@ def send_image(tg_bot_token, tg_channel_id, image_path):
 
     with open(image_path, 'rb') as image:
         bot.send_photo(tg_channel_id, image)
-
-
-def main():
-    bot = ImagePosterBot().bot
-    print(bot.get_me())
-
-
-if __name__ == '__main__':
-    main()
