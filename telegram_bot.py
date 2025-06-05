3f8223663413315e49b0b788558bea722f7f2ea6
olenevoid <mazekunov@gmail.com>
2025-06-05 15:57:18 +0500

    Delete ImagePosterBot class

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,18 +3,6 @@ from dotenv import load_dotenv
 from telegram import Bot
 
 
-class ImagePosterBot():
-    def __init__(self):
-        load_dotenv()
-        self.tg_bot_token = environ['TG_BOT_TOKEN']
-        self.tg_channel_id = environ['TG_CHANNEL_ID']
-        self.bot = Bot(self.tg_bot_token)
-
-    def send_image(self, image_path):
-        image = open(image_path, 'rb')
-        self.bot.send_photo(chat_id=self.tg_channel_id, photo=image)
-
-
 def send_image(tg_bot_token, tg_channel_id, image_path):
     bot = Bot(tg_bot_token)
 
